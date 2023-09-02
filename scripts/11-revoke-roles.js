a43b5c2c809a0a1e76219fa380c9427888fa9959
JeisonNovoa <jeisonnovoa7@gmail.com>
2023-09-01 20:08:37 -0500

    update project is completed

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,22 @@
+import sdk from "./1-initialize-sdk.js";
+
+(async () => {
+  try {
+    const token = await sdk.getContract("0x9CD97b01e1E042cE63656E4E6EdC75f2610Ff323", "token");
+    // Log the current roles.
+    const allRoles = await token.roles.getAll();
+
+    console.log("👀 Roles that exist right now:", allRoles);
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await token.roles.setAll({ admin: [], minter: [] });
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await token.roles.getAll()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
